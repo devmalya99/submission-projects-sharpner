ca7abf5d7731ecefbee366fd055132eb5f9e3f17
devmalya99 <debmalyamazumdar99@gmail.com>
2024-02-29 13:28:23 +0530

    06--redcers o oadd and delete expenses

--- 06-Expense Tracker/paisa file/src/CardsComponents/AddExpenseCard.jsx
+++ 06-Expense Tracker/paisa file/src/CardsComponents/AddExpenseCard.jsx
@@ -1,46 +1,75 @@
-import React from 'react';
-
+import React, {useContext , useState} from 'react'
+import {ExpenseContext} from '../Home/Home'
 const AddExpenseCard = () => {
+
+  const {dispatch} = useContext(ExpenseContext)
+  const [title, setTitle] = useState('')
+  const [amount, setAmount] = useState('')
+  const [category, setCategory] = useState('')
+
+  const generateId = () => {
+    const rawId = `${title}${amount}${category}`;
+    const uniqueId = rawId.replace(/\s+/g, '-').toLowerCase()
+    return `${uniqueId}-${Math.random().toString(36).slice(2)}`;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!title || !amount || !category){
+      alert ('please enter all the fields');
+      return
+    }
+    dispatch({ type: 'ADD_EXPENSE', payload: { title, amount, category, id: generateId()} }); // or a unique key generator
+    setTitle('');
+    setAmount('');
+    setCategory('');
+  }
+
   return (
     <div className='container mx-auto mt-10 max-w-md shadow-lg'>
-      <div className='border-2 border-gray-200 p-5 rounded-md bg-white shadow-sm'>
-        <form>
-
-          <div className='mb-4'>
-            <input
-              type='text'
-              placeholder='Expense Heading'
-              className='w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300'
-            />
-          </div>
-
-          <div className='mb-4'>
-            <input
-              type='number'
-              placeholder='Expense Amount'
-              className='w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300'
-            />
-          </div>
-
-          <div className='mb-4'>
-            <select className='form-select block w-full px-3 py-2 rounded-md'>
-              <option>Expense Category</option>
-              <option value={'Food'}>Food</option>
-              <option value={'Utilities'}>Utilities</option>
-              <option value={'Entertainment'}>Entertainment</option>
-              <option value={'Gadgets'}>Gadgets</option>
-            </select>
-          </div>
-
-          <div>
-            <button className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-              Add Expense
-            </button>
-          </div>
-
-        </form>
-      </div>
+    <div className='border-2 border-gray-200 p-5 rounded-md bg-white shadow-sm'>
+      <form onSubmit={handleSubmit}>
+        <label className='mb-4'>
+          Expense Heading
+          <input
+          onChange={e=>setTitle(e.target.value)}
+          value={title}
+            type='text'
+            placeholder='...Title'
+            className='w-full bg-blue-200 px-3 py-2 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300'
+          />
+        </label>
+        <label className='mb-4'>
+          Expense Amount
+          <input
+           onChange={e=>setAmount(e.target.value)}
+           value={amount}
+            type='number'
+            placeholder='...Amount'
+            className='w-full px-3 bg-blue-200 py-2 text-gray-900 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300'
+          />
+        </label>
+        <div className='mb-4'>
+          <label className='mb-2'>
+            Expense Category
+          </label>
+          <select 
+          onChange={e=>setCategory(e.target.value)}
+          value={category}
+          className='block w-full bg-blue-200 px-3 py-2 rounded-md'>
+            <option disabled selected value={''}>Select Category</option>
+            <option value={'Food'}>Food</option>
+            <option value={'Utilities'}>Utilities</option>
+            <option value={'Entertainment'}>Entertainment</option>
+            <option value={'Gadgets'}>Gadgets</option>
+          </select>
+        </div>
+        <button className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' aria-label="Add Expense">
+          Add Expense
+        </button>
+      </form>
     </div>
+  </div>
   );
 }
 
--- 06-Expense Tracker/paisa file/src/Home/Home.jsx
+++ 06-Expense Tracker/paisa file/src/Home/Home.jsx
@@ -3,7 +3,33 @@ import { Link , useNavigate} from 'react-router-dom';
 import { getDatabase, ref, get } from "firebase/database";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import AddExpenseCard from '../CardsComponents/AddExpenseCard';
+import {useReducer} from 'react';
+import { createContext } from 'react'
+
+const initialState = {
+  expenses:[],
+}
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'ADD_EXPENSE':
+      return {...state,expenses:[...state.expenses,action.payload]}
+     
+    case 'DELETE_EXPENSE':
+      return {...state, expenses: state.expenses.filter((expense) => expense.id !== action.id)}
+    default:
+      return state
+  
+    }
+
+}
+
+export const ExpenseContext = createContext()
+
 const Home = () => {
+
+
+  const [state , dispatch] = useReducer(reducer,initialState)
   const [isProfileComplete, setProfileComplete] = useState(false);
   const Navigate = useNavigate();
   useEffect(() => {
@@ -18,11 +44,33 @@ const Home = () => {
         }
       }
     })
+    console.log("state is",state)
   },[])
 
   const logout =()=>{
     localStorage.removeItem('user')
     Navigate('/')
+  }
+
+  const ExpenseItem = ({title,amount,category,id, dispatch})=>{
+    return(
+      <div className="md:flex md:items-center md:justify-between">
+        <div className="flex justify-start items-center w-full md:w-auto">
+         <li>
+      <h3>{title}</h3>
+      <p>Amount: {amount}</p>
+      <p>Category: {category}</p>
+    </li>
+
+    <button 
+    onClick={() => dispatch({ type: 'DELETE_EXPENSE', id })}
+    className='text-white-500  hover:font-semibold  ml-8 p-2  bg-red-500 hover:bg-red-700'>
+      Delete
+    </button>
+    </div>
+      </div>
+     
+    )
   }
 
   return (
@@ -58,26 +106,24 @@ const Home = () => {
     </div>
   </header>
 
-  <AddExpenseCard/>
+  <ExpenseContext.Provider value={{state,dispatch}}>
+    <AddExpenseCard/>
+  </ExpenseContext.Provider>
+
+  
   <div className="display div container mx-auto mt-10 max-w-md shadow-lg">
   <div className="border-2 border-gray-200 p-5 rounded-md bg-white shadow-sm">
   <ul>
-            <li>
-                Item 1
-            </li>
-            <li>
-                Item 1
-            </li>
-            <li>
-                Item 1
-            </li>
-            <li>
-                Item 1
-            </li>
-            <li>
-                Item 1
-            </li>
-        </ul>
+  {
+    state.expenses.map((expense)=>{
+      return <ExpenseItem 
+      key={expense.id}
+      dispatch={dispatch}
+      {...expense} />
+    })
+  }
+           
+ </ul>
 
   </div>
   
